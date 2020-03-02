7f919df86b527f0bb4cc9edd421dc3a15e680d32
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2020-03-02 16:15:26 -0700

    fixed broken code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var Config = {
 
 
 // On click event
-$("#tester").on("click", function() {
+$("#submitFrm-btn").on("click", function() {
 
     
 
@@ -56,65 +56,5 @@ firebase.database().ref().on("child_added", function (snapshot) {
     var dbProdDetails = snapshot.val().ProdDetails
 });
 
-
-
-
-// console.log("AppJS is running....");
-
-
-//   // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyBMh6v1EQCofg7NqDRkL_-tJXpLGB0Kp_k",
-//     authDomain: "cblumberco.firebaseapp.com",
-//     databaseURL: "https://cblumberco.firebaseio.com",
-//     projectId: "cblumberco",
-//     storageBucket: "cblumberco.appspot.com",
-//     messagingSenderId: "75242589579",
-//     appId: "1:75242589579:web:522dd999a2000ad8fcb38e"
-//   };
-//   // Initialize Firebase
-// //   firebase.initializeApp(firebaseConfig);
-
-
-// //   var database = firebase.database();
-
-
-
-
-//   $("submitFrm-btn").on("click", function() {
-
-//     console.log("submit fired off")
-
-//         var name = $("#name-input").val().trim();
-//           var email = $("#email-input").val().trim();
-//           var city = $("#city-input").val().trim();
-//           var state = $("#state-input").val().trim();
-//           var zipCode = $("#zip-input").val().trim();
-//           var product = $("#product-input").val().trim();
-//           var dimentions = 4("#dimensions-input").val().trim();
-//           var prodDetail = $("#prodDet-input").val().trim()
-      
-//           database.ref().push({
-//               dbName: name,
-//               dbEmail: email,
-//               dbCity: city,
-//               dbState: state,
-//               dbZip: zipCode,
-//               dbProduct: product,
-//               dbDimentions: dimentions,
-//               dbProdDetails: prodDetail,
-//           })
-//       })
-      
-//       firebase.database().ref().on("child_added", function (snapshot) {
-//           var dbName = snapshot.val().dbName;
-//           var dbEmail = snapshot.val().dbEmail;
-//           var dbCity = snapshot.val().dbCity;
-//           var dbState = snapshot.val().dbState;
-//           var dbZip = snapshot.val().dbZip
-//           var dbProduct = snapshot.val().dbProduct;
-//           var dbDimentions = snapshot.val().dbDimentions;
-//           var dbProdDetails = snapshot.val().dbProdDetails
-//   });
 
 
